c232412d028c17c8ccb9ca56f4fafc274de51a39
polyphemus <rolfmorel@gmail.com>
2014-03-27 18:48:43 +0100

    Add more information for push to pypi

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import os
 from setuptools import setup, find_packages
 
 import macht
@@ -8,12 +9,30 @@ import macht
 setup(
     name='macht',
     version=macht.__version__,
+    description="A 2048 clone in python with terminal ui frontend",
+    long_description=open(os.path.join(os.path.dirname(__file__),
+                          'README.md')).read(),
     author="Rolf Morel",
     author_email="rolfmorel@gmail.com",
+    url="https://github.com/polyphemus/macht",
     packages=find_packages(exclude=["tests"]),
     license='LGPLv3',
     install_requires=['blessed', 'enum34'],
-    platforms=['Linux'],
     entry_points={'console_scripts': ('macht = macht.term:main')},
-    description=("A 2048 clone in python with terminal ui frontend")
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: '
+        'GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Games/Entertainment :: Puzzle Games',
+        'Topic :: Terminals'
+        )
 )
