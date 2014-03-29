469da85435c010a4fc99ed1b69762acbb3a34ea8
polyphemus <rolfmorel@gmail.com>
2014-03-29 14:42:06 +0100

    Change release level to 'final'
    This is primarily to let pip install macht as non-development package.

--- macht/__init__.py
+++ macht/__init__.py
@@ -1,9 +1,8 @@
 from collections import namedtuple
 
 components = 'major, minor, micro, releaselevel, serial'
-values = (0, 1, 0, 'beta', 0)
+values = (0, 1, 0, 'final', 0)
 __version_info__ = namedtuple('__version_info__', components)(*values)
 __version__ = (str(__version_info__.major) + '.' +
-               str(__version_info__.minor) + '-' +
-               __version_info__.releaselevel)
+               str(__version_info__.minor))
 del components, values
