cdc473e1fa7c5faed2114eb3b6ecbf6dfc6c5245
polyphemus <rolfmorel@gmail.com>
2014-04-06 13:38:26 +0200

    Increment version

--- macht/__init__.py
+++ macht/__init__.py
@@ -1,8 +1,9 @@
 from collections import namedtuple
 
 components = 'major, minor, micro, releaselevel, serial'
-values = (0, 1, 0, 'final', 0)
+values = (0, 1, 1, 'final', 0)
 __version_info__ = namedtuple('__version_info__', components)(*values)
 __version__ = (str(__version_info__.major) + '.' +
-               str(__version_info__.minor))
+               str(__version_info__.minor) + '.' +
+               str(__version_info__.micro))
 del components, values
